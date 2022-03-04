bf195fcb8f3e32c87382cbc2d40511accef233b8
Tony Greeley <tonygreeley@Tonys-iMac.local>
2022-03-03 19:23:52 -0600

    Completed subtract, multiply, and divide functionality of calc.

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -4,13 +4,47 @@ let currentResult = defaultResult;
 let calculationDescription;
 
 // Functions
-// Addition - adds the currentResult and userInput together and reassigns that value to currentResult and updates DOM 
+// Get the number user entered into calculator and convert to a number
+const getUserInput = () => {
+  return parseFloat(userInput.value)
+}
+
+// Creates and writes output
+const createAndWriteOutput = (operator, resultBeforeCalc, calcNumber) => {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+// Functions to perform the mathematic operations of addition, subtraction, multiplication, and division 
 const add = () => {
-  currentResult = currentResult + userInput.value;
-  outputResult(currentResult, calculationDescription);
-};
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber);
+  }
 
-// When add button is clicked run the add function
+const subtract = () => {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+const multiply = () => {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+const divide = () => {
+  const enteredNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
+// Event listeners to respond to button clicks
 addBtn.addEventListener('click', add);
-
-
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
