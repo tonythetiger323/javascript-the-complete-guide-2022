bf031bdcbad7e9d96f551332bdf242235928633d
Tony Greeley <tonygreeley@Tonys-iMac.local>
2022-04-02 16:58:26 -0500

    Completed module

---
+++ behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,22 @@
+// let name = 'Tony';
+
+// if (name === 'Tony') {
+//   let hobbies = ['Sports', 'Coding'];
+//   console.log(hobbies)
+// }
+
+// const greet = () => {
+//   let age = 30;
+//   let name = 'David';
+//   console.log(name, age, hobbies);
+// }
+
+// console.log(name, hobbies);
+
+// greet();
+'use strict';
+
+const userName = 'Tony';
+var undefined = 5; 
+
+console.log(userName);
